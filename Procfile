33ea0d7699623d36e1ef14b1b8b747b61532939e
wandrad3 <wesleyandrade298@gmail.com>
2024-04-22 12:05:19 -0300

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar build/libs/lab-dio-spring-railway-0.0.1-SNAPSHOT.jar
---
+++ src/test/java/me/dio/labdiospringrailway/LabDioSpringRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.labdiospringrailway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabDioSpringRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
