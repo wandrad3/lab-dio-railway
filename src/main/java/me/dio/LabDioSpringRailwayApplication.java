e1bd910791cd3edeec888a1a68add4ba57507818
wandrad3 <wesleyandrade298@gmail.com>
2024-04-22 13:45:21 -0300

    Bug Fix-CORS

--- src/main/java/me/dio/LabDioSpringRailwayApplication.java
+++ src/main/java/me/dio/LabDioSpringRailwayApplication.java
@@ -3,6 +3,11 @@ package me.dio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers= {@Server(url = "/",description = "Default Server URL")})
+
 @SpringBootApplication
 public class LabDioSpringRailwayApplication {
 
